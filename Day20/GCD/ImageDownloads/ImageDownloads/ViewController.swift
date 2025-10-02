619adfdc4b7e2965662dfbb4b8aed398362a39c3
gurman-man <maksikkoval22@gmail.com>
2025-10-03 01:25:04 +0300

    Day 20 - GrandCentralDispatch

---
+++ Day20/GCD/ImageDownloads/ImageDownloads/ViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ViewController.swift
+//  ImageDownloads
+//
+//  Created by mac on 02.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    let imageURLs = [
+        URL(string: "https://picsum.photos/1920/1080")!,
+        URL(string: "https://picsum.photos/1920/1080")!,
+        URL(string: "https://picsum.photos/1920/1080")!
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    // MARK: - GCD
+    func loadImagesWithGCD() {
+        let imageViews = [imageView1, imageView2, imageView3]
+        activityIndicator.startAnimating()
+        view.isUserInteractionEnabled = false
+        let startTime = Date()
+        
+        let dispatchGroup = DispatchGroup()
+        
+        for (index, url) in imageURLs.enumerated() {
+            dispatchGroup.enter()
+            
+            // Використовуємо URLSession замість Data(contentsOf:)
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        if index < imageViews.count { // безпечний доступ
+                            imageViews[index]?.image = image
+                        }
+                        dispatchGroup.leave()
+                    }
+                } else {
+                    print("GCD error: \(error?.localizedDescription ?? "unknown")")
+                    dispatchGroup.leave()
+                }
+            }.resume()
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            self.activityIndicator.stopAnimating()
+            self.view.isUserInteractionEnabled = true
+            let endTime = Date()
+            let duration = endTime.timeIntervalSince(startTime)
+            print("GCD завантаження тривало \(duration) секунд")
+        }
+    }
+    
+    // MARK: - Async/Await
+    func loadImagesWithAsyncAwait() async {
+        let imageViews = [imageView1, imageView2, imageView3]
+        await MainActor.run {
+            activityIndicator.startAnimating()
+            view.isUserInteractionEnabled = false
+        }
+        let startTime = Date()
+        
+        await withTaskGroup(of: (Int, UIImage?).self) { group in
+            for (index, url) in imageURLs.enumerated() {
+                group.addTask {
+                    do {
+                        let (data, _) = try await URLSession.shared.data(from: url)
+                        let image = UIImage(data: data)
+                        return (index, image)
+                    } catch {
+                        print("Async error: \(error)")
+                        return (index, nil)
+                    }
+                }
+            }
+            
+            for await (index, image) in group {
+                await MainActor.run {
+                    if index < imageViews.count {
+                        imageViews[index]?.image = image
+                    }
+                }
+            }
+        }
+        
+        await MainActor.run {
+            activityIndicator.stopAnimating()
+            view.isUserInteractionEnabled = true
+            let endTime = Date()
+            let duration = endTime.timeIntervalSince(startTime)
+            print("Async/Await завантаження тривало \(duration) секунд")
+        }
+    }
+    
+    
+    // MARK: - IBActions
+    @IBAction func downloadWithGCDTapped(_ sender: UIButton) {
+        loadImagesWithGCD()
+    }
+    
+    @IBAction func downloadWithAsyncAwaitTapped(_ sender: UIButton) {
+        Task {
+            await loadImagesWithAsyncAwait()
+        }
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+        [imageView1, imageView2, imageView3].forEach { $0?.image = nil }
+    }
+    
+}
+
